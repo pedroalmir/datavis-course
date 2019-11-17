77bf503eecb140a87b138de75f7e077446bc5a85
Pedro Almir <petrus.cc@gmail.com>
2019-11-17 00:10:38 -0300

    Ajustes dashboard 19

--- trabalho_final/dashboard/notebook/22466d58aaebf35f@2160.js
+++ trabalho_final/dashboard/notebook/22466d58aaebf35f@2160.js
@@ -692,13 +692,10 @@ DOM.download(() => serialize(treeChartSVG.svg[0][0]), undefined, "Save as SVG")
   main.variable(observer("treeChart")).define("treeChart", ["postsMalletModel","tNumberSelect","d3t","treeChartSVG","d3"], function(postsMalletModel,tNumberSelect,d3t,treeChartSVG,d3)
 {
 // ************** Generate the tree diagram	 *****************
-  var treeData = postsMalletModel.myTrees['topic' + $(this).children("option:selected").val()]
-  
-  let treeChartWidth = Math.floor($($('#treeContainer').parent().get(0)).width());
-  
   var i = 0, duration = 1000, root;
-  var height = 1000, width = treeChartWidth;
+  var height = 1000, width = 720;
   var margin = ({top: 20, right: 120, bottom: 20, left: 120});
+  var treeData = postsMalletModel.myTrees['topic' + tNumberSelect]
 
   var tree = d3t.layout.tree().size([height, width]);
   var diagonal = d3t.svg.diagonal().projection(function(d) { return [d.y, d.x]; });
@@ -715,7 +712,8 @@ DOM.download(() => serialize(treeChartSVG.svg[0][0]), undefined, "Save as SVG")
   root.y0 = 0;
   
   update(root);
-
+  
+  treeChartSVG.svg = svg;
 
   function update(source) {
     // Compute the new tree layout.
@@ -829,6 +827,147 @@ DOM.download(() => serialize(treeChartSVG.svg[0][0]), undefined, "Save as SVG")
     }
     update(d);
   }
+  //###############################################
+  //###############################################
+  //###############################################
+  $("#topicNumberInput").change(function() {
+    var tNumberSelect = $(this).children("option:selected").val();
+    var i = 0, duration = 1000, root;
+  var height = 1000, width = 720;
+  var margin = ({top: 20, right: 120, bottom: 20, left: 120});
+  var treeData = postsMalletModel.myTrees['topic' + tNumberSelect]
+
+  var tree = d3t.layout.tree().size([height, width]);
+  var diagonal = d3t.svg.diagonal().projection(function(d) { return [d.y, d.x]; });
+  
+  d3t.select("#treeContainer").html("");
+  var svg = d3t.select("#treeContainer").append("svg")
+	  .attr("width", width + margin.right + margin.left)
+	  .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+	  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  root = treeData[0];
+  root.x0 = height / 2;
+  root.y0 = 0;
+  
+  update(root);
+  
+  treeChartSVG.svg = svg;
+
+  function update(source) {
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root), links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 120; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node").data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+	    .attr("class", d => ("node " + d.name.replace(' ', '').toLowerCase().trim()))
+	    .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+	    .on("click", click);
+
+    nodeEnter.append("circle")
+	    .attr("r", 1e-6)
+	    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+	    .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
+	    .attr("dy", ".35em")
+	    .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+	    .text(function(d) { return d.name; })
+	    .style("fill-opacity", 1e-6)
+      .call(wrap, 150);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition().duration(duration)
+	    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle").attr("r", 10)
+      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text").style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition().duration(duration)
+	    .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+	    .remove();
+
+    nodeExit.select("circle").attr("r", 1e-6);
+    nodeExit.select("text").style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link").data(links, function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+	    .attr("class", "link")
+	    .attr("d", function(d) {
+		    var o = {x: source.x0, y: source.y0};
+		    return diagonal({source: o, target: o});
+	    });
+
+    // Transition links to their new position.
+    link.transition().duration(duration).attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition().duration(duration)
+	    .attr("d", function(d) {
+		    var o = {x: source.x, y: source.y};
+		    return diagonal({source: o, target: o});
+	    })
+	  .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+	    d.x0 = d.x;
+	    d.y0 = d.y;
+    });
+  }
+  
+  function wrap(text, twidth) {
+    text.each(function() {
+      
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word, line = [], 
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy"));
+      
+      while (word = words.pop()) {
+        line.push(word);
+        if(line.join(" ").length > 20 && line.join(" ") != 'diagnostic order schedule'){
+          var tspan = text.text(null).append("tspan").attr("x", -15).attr("y", y).attr("dy", "-0.3em")
+          tspan.text(line.join(" "));
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan")
+            .attr("x", -15).attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        }
+      }
+    });
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+	    d._children = d.children;
+	    d.children = null;
+    } else {
+	    d.children = d._children;
+	    d._children = null;
+    }
+    update(d);
+  }
+  });
   //return svg.node();
 }
 );
